bd124a84be87accdc65dfb1e3744680b565df998
Hebert Rocha <70274409+Hebert324@users.noreply.github.com>
2021-09-09 08:15:35 -0300

    Update styles.js

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -1,5 +1,15 @@
 import styled from "styled-components";
 
 export const Header = styled.div`
-    
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .logo img{
+        width: 100px;
+    }
+
+    .user img {
+        width: 70px;
+    }
 `
