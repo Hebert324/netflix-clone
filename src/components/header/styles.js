790391ba237174c908b9800e7649c99d17f67406
Hebert Rocha <70274409+Hebert324@users.noreply.github.com>
2021-09-09 14:05:51 -0300

    setting the header

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -1,15 +1,30 @@
 import styled from "styled-components";
 
 export const Header = styled.div`
+    position: fixed;
+    z-index: 999;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 70px;
     display: flex;
     align-items: center;
     justify-content: space-between;
+    padding: 0 30px;
+    background: transparent;
 
     .logo img{
-        width: 100px;
+        width: 90px;
     }
 
     .user img {
-        width: 70px;
+        width: 40px;
+        border-radius: 3px;
+    }
+`
+
+export const Black = styled.div`
+    ${Header}.black {
+        background: #111;
     }
 `
