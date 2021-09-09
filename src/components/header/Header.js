94ead826f3e5d5ffe9a9c512ba791886f2f52b4d
Hebert Rocha <70274409+Hebert324@users.noreply.github.com>
2021-09-09 10:01:54 -0300

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,10 +5,10 @@ export default function header() {
   return(
     <Header>
       <div className="logo">
-      <a href="#"><img src='https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png' alt='logo' /></a>
+      <a href="/" rel="noreferrer"><img src='https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png' alt='logo' /></a>
       </div>
       <div className="user">
-      <a href="#"><img src='https://avatars.githubusercontent.com/u/70274409?v=4' alt='user' /></a>
+      <a href="/" rel="noreferrer"><img src='https://avatars.githubusercontent.com/u/70274409?v=4' alt='user' /></a>
       </div>
     </Header>
   )
